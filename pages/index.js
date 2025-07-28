5d2703eb395a8ac569376c05fb6a13f1d9a16803
adang-sy <76038089+adang-sy@users.noreply.github.com>
2025-07-28 11:09:30 +0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,13 +13,13 @@ export default function Home() {
   return (
     <main className="max-w-3xl mx-auto p-4">
       <h1 className="text-4xl font-bold mb-8">There is no free lunch in the world, thank you for browsing</h1>
-      {posts.map((post) => (
-        <Link key={post.slug} href={`/posts/${post.slug}`}>
-          <a className="block mb-4 p-4 rounded-xl shadow hover:bg-gray-50">
-            <h2 className="text-2xl font-semibold">{post.title}</h2>
-            <p className="text-gray-600">{post.date}</p>
-          </a>
-        </Link>
+      //{posts.map((post) => (
+        //<Link key={post.slug} href={`/posts/${post.slug}`}>
+          //<a className="block mb-4 p-4 rounded-xl shadow hover:bg-gray-50">
+            //<h2 className="text-2xl font-semibold">{post.title}</h2>
+            //<p className="text-gray-600">{post.date}</p>
+          //</a>
+        //</Link>
       ))}
     </main>
   );
